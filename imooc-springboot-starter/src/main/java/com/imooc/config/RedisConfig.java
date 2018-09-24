a6499f18f056e4b4e72c6a44aaf02206c70a1b1c
LAPTOP-5MASP1C4\yang <763216917@qq.com>
2018-09-24 12:18:55 +0800

    five

--- imooc-springboot-starter/src/main/java/com/imooc/config/RedisConfig.java
+++ imooc-springboot-starter/src/main/java/com/imooc/config/RedisConfig.java
@@ -3,5 +3,6 @@ package com.imooc.config;
 public class RedisConfig {
 	public static void main(String[] args) {
 		System.out.println("hello git13134");
+		System.out.println("hello git13134");
 	}
 }
